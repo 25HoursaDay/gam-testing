04461c28768195872210df655ae68d51e0bac6fc
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-06-08 20:17:53 -0700

    Update request.js

--- wordle-clone/request.js
+++ wordle-clone/request.js
@@ -1,4 +1,5 @@
 var fe;
+var wordwwd = "cum";
 
 function loadXMLDoc() {
   var xhttp = new XMLHttpRequest();
@@ -6,7 +7,14 @@ function loadXMLDoc() {
     if (this.readyState == 4 && this.status == 200) {
       fe =
         this.responseText.trim().split("\n");
-      console.log(fe[2].toString().trim());
+
+      for (var i = 0; i < fe.length; i++) {
+        if (fe[i].toString().trim() == wordwwd) {
+          console.log(fe[i].toString().trim());
+          console.log(i);
+          break;
+        }
+      }
     }
   };
   xhttp.open("GET", "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt", true);

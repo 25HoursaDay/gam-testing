a4246329e27eea59805941a36e146f268d74e972
25HoursaDay <98925934+25HoursaDay@users.noreply.github.com>
2023-06-08 20:09:00 -0700

    Update request.js

--- wordle-clone/request.js
+++ wordle-clone/request.js
@@ -6,7 +6,7 @@ function loadXMLDoc() {
     if (this.readyState == 4 && this.status == 200) {
       fe =
         this.responseText.trim().split("\n");
-      console.log(fe);
+      console.log(fe[2].toString().trim());
     }
   };
   xhttp.open("GET", "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt", true);
